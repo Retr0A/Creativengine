397cc8883e8d591442c9688b98b9bd97b32fcda8
Retr0A <alexxretr0a@gmail.com>
2022-07-24 13:07:38 +0300

    Triangle Changed To Rectangle

--- Creativengine/src/Creativengine/EntryPoint.cpp
+++ Creativengine/src/Creativengine/EntryPoint.cpp
@@ -108,21 +108,30 @@ CREATIVENGINE_API void Run()
 		return;
 	}
 
-	float positions[6] = {
+	float positions[] = {
 			-0.5f, -0.5f,
-			 0.0f,  0.5f,
-			 0.5f, -0.5f
+			 0.5f, -0.5f,
+			 0.5f,  0.5f,
+			-0.5f,  0.5f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -132,7 +141,7 @@ CREATIVENGINE_API void Run()
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glClearColor(0.0f, 0.3f, 0.4f, 1.0f);
 
