3e3634050119d28056e4df5237a492d51b82aa95
Retr0A <Retr0A@DESKTOP-LB0MQSG>
2023-05-18 20:44:44 +0300

    Added Window

---
+++ Creativengine/src/EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using Creativengine.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creativengine
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Window window = new Window(1080, 720, "Creativengine");
+
+            window.CreateForm();
+
+            Application.Run(window.GetForm());
+        }
+    }
+}
---
+++ Creativengine/src/Graphics/Window.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creativengine.Graphics
+{
+    public class Window
+    {
+        private int m_Width, m_Height;
+        private string m_Title;
+
+        private Form m_Form;
+
+        public Window(int width, int height, string title)
+        {
+            m_Width = width;
+            m_Height = height;
+            m_Title = title;
+        }
+
+        public void CreateForm()
+        {
+            m_Form = new Form();
+
+            m_Form.Width = m_Width;
+            m_Form.Height = m_Height;
+            m_Form.Text = m_Title;
+
+            m_Form.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        public Form GetForm() { return m_Form; }
+    }
+}
